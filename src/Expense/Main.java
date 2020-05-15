966aa2b6166ae95a9c6482f66e96121d01331eb7
Noah <noahlee1013@gmail.com>
2020-05-14 20:19:20 -0600

    This is my commit.

--- src/Expense/Main.java
+++ src/Expense/Main.java
@@ -1,9 +1,11 @@
+
 package Expense;
 
-public class Main {
+public class HelloFromNoah {
 
     public static void main(String[] args) {
-	// write your code here
-	    //test comment
+        // write your code here
+        System.out.println("Hello there!");
+        //test comment
     }
 }
