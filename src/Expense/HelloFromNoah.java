a920f472b16bc82bac7ab762be9ef9ebd538e62d
ryan ovard <rjamesovard@gmail.com>
2020-05-16 12:58:48 -0600

    Ryan edit-1

--- src/Expense/HelloFromNoah.java
+++ src/Expense/HelloFromNoah.java
@@ -8,6 +8,7 @@ public class HelloFromNoah {
             System.out.println("Hello there!");
 
             System.out.println("Hello Noah, I'm your team member Will.");
+            System.out.println("Hello Niah, I'm your team member Ryan.")
         //test comment
     }
 }
