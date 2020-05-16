eec8eb1422d4bf808da6124bf21d6ad886555f6d
willbrown600 <bro18085@byui.edu>
2020-05-16 21:04:18 +1000

    update from Will

--- src/Expense/HelloFromNoah.java
+++ src/Expense/HelloFromNoah.java
@@ -6,6 +6,8 @@ public class HelloFromNoah {
     public static void main(String[] args) {
         //write your code here
             System.out.println("Hello there!");
+
+            System.out.println("Hello Noah, I'm your team member Will.");
         //test comment
     }
 }
