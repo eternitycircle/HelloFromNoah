d2dc3da65781e8db1b48a8e567951a2537850ce6
Noah <noahlee1013@gmail.com>
2020-05-16 23:58:45 -0600

    Committing again.

--- src/Expense/HelloFromNoah.java
+++ src/Expense/HelloFromNoah.java
@@ -5,10 +5,10 @@ public class HelloFromNoah {
 
     public static void main(String[] args) {
         //write your code here
-            System.out.println("Hello there!");
+            System.out.println("Hello, my name is Noah.");
 
             System.out.println("Hello Noah, I'm your team member Will.");
-            System.out.println("Hello Niah, I'm your team member Ryan.")
+            System.out.println("Hello Noah, I'm your team member Ryan.");
         //test comment
     }
 }
